617e4b457fdff887ffa2ed4d6fa73f4bc3b0a687
HamedDiaby <hameddiaby.koumba@gmail.com>
2024-04-19 14:39:14 +0200

    add chats

--- src/pages/Home/Home.component.tsx
+++ src/pages/Home/Home.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import './styles.css';
 import { IHome } from "./interfaces";
 import { useTranslation } from "@utilities/hooks";
@@ -6,26 +6,243 @@ import { Dashboard } from "@dashboardWrappers";
 import { Section } from "@components";
 import { NavigationPathsEnum } from "@utilities/enums";
 
+import { Chart } from 'primereact/chart';
+
 export const HomeComponent:React.FC<IHome> = ({
 
 })=> {
 
     const translate = useTranslation();
 
+    const renderLineChart = useCallback(()=> {
+
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+        const data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    tension: 0.4,
+                    borderColor: documentStyle.getPropertyValue('--blue-500')
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderDash: [5, 5],
+                    tension: 0.4,
+                    borderColor: documentStyle.getPropertyValue('--teal-500')
+                },
+                {
+                    label: 'Third Dataset',
+                    data: [12, 51, 62, 33, 21, 62, 45],
+                    fill: true,
+                    borderColor: documentStyle.getPropertyValue('--orange-500'),
+                    tension: 0.4,
+                    backgroundColor: 'rgba(255,167,38,0.2)'
+                }
+            ]
+        };
+        const options = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.6,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder
+                    }
+                }
+            }
+        };
+
+        return (
+            <Section className="ad_chart_section_container">
+                <Chart type="line" data={data} options={options} />
+            </Section>
+        )
+    }, []);
+
+    const renderVerticalBarChart = useCallback(()=> {
+
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+        const data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+                    borderColor: documentStyle.getPropertyValue('--blue-500'),
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: documentStyle.getPropertyValue('--pink-500'),
+                    borderColor: documentStyle.getPropertyValue('--pink-500'),
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+        const options = {
+            maintainAspectRatio: false,
+            aspectRatio: 0.8,
+            plugins: {
+                legend: {
+                    labels: {
+                        fontColor: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                        font: {
+                            weight: 500
+                        }
+                    },
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+            }
+        };
+
+        return (
+            <Section className="ad_chart_section_container">
+                <Chart type="bar" data={data} options={options} />
+            </Section>
+        )
+    }, []);
+
+    const renderPieChart = useCallback(()=> {
+
+        const documentStyle = getComputedStyle(document.documentElement);
+        const data = {
+            labels: ['A', 'B', 'C'],
+            datasets: [
+                {
+                    data: [540, 325, 702],
+                    backgroundColor: [
+                        documentStyle.getPropertyValue('--blue-500'), 
+                        documentStyle.getPropertyValue('--yellow-500'), 
+                        documentStyle.getPropertyValue('--green-500')
+                    ],
+                    hoverBackgroundColor: [
+                        documentStyle.getPropertyValue('--blue-400'), 
+                        documentStyle.getPropertyValue('--yellow-400'), 
+                        documentStyle.getPropertyValue('--green-400')
+                    ]
+                }
+            ]
+        }
+        const options = {
+            plugins: {
+                legend: {
+                    labels: {
+                        usePointStyle: true
+                    }
+                }
+            }
+        };
+
+        return (
+            <Section className="ad_chart_section_container ad_chart_width">
+                <Chart 
+                    type="pie" 
+                    data={data} 
+                    options={options}
+                    className="ad_chart_width"
+                />
+            </Section>
+        )
+    }, []);
+
+    const renderDoughnutChart = useCallback(()=> {
+
+        const documentStyle = getComputedStyle(document.documentElement);
+        const data = {
+            labels: ['A', 'B', 'C'],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        documentStyle.getPropertyValue('--blue-500'), 
+                        documentStyle.getPropertyValue('--yellow-500'), 
+                        documentStyle.getPropertyValue('--green-500')
+                    ],
+                    hoverBackgroundColor: [
+                        documentStyle.getPropertyValue('--blue-400'), 
+                        documentStyle.getPropertyValue('--yellow-400'), 
+                        documentStyle.getPropertyValue('--green-400')
+                    ]
+                }
+            ]
+        };
+        const options = {
+            cutout: '60%'
+        };
+
+        return (
+            <Section className="ad_chart_section_container ad_chart_width">
+                <Chart 
+                    type="doughnut" 
+                    data={data} 
+                    options={options}
+                    className="ad_chart_width"
+                />
+            </Section>
+        )
+    }, []);
+
     return (
         <Dashboard activePagePath={NavigationPathsEnum.HOME}>
             <div>
-                <Section>
-                    Home
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod deserunt praesentium vero enim saepe dolor cumque ut quam harum, dicta accusantium facilis temporibus totam quis excepturi possimus! Quam, distinctio quos.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ab ipsum, cum eos facilis minus vitae rem provident maiores nemo praesentium quasi quos rerum aliquam mollitia deleniti perspiciatis, impedit suscipit.lore
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, doloremque sapiente officiis facere consequatur dolor asperiores a at porro ducimus! Consequuntur veniam quidem laborum mollitia, hic maiores eligendi. Libero, explicabo.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis iusto quod libero explicabo facilis commodi autem delectus vero? Ipsa laboriosam assumenda voluptatum natus aliquam? Voluptate, voluptas nihil! Officia, esse unde.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum tenetur dolor asperiores fugit libero eligendi labore, id ut. At eos officia quasi sed libero quis modi obcaecati optio quo. Modi.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quod dolores, debitis libero, beatae velit quia laboriosam quidem consectetur, impedit eaque natus qui rem. Debitis illo consectetur eum consequatur neque?
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, ipsa odit pariatur numquam soluta minima quasi! Maiores deleniti ratione quasi, vero explicabo, aliquam earum, debitis repellat laborum tenetur labore quas!
-                    
-                </Section>
+                {renderLineChart()}
+
+                {renderVerticalBarChart()}
+
+                <div className="ad_group_chart_container">
+                    {renderPieChart()}
+
+                    {renderDoughnutChart()}
+                </div>
             </div>
         </Dashboard>
     )
